bdcf15845fc457d7b740dc4017f5f25d0dbc4f0e
Mahzeb Saleem <109290300+Mahzeb-Saleem@users.noreply.github.com>
2022-07-14 23:11:37 +0500

    Add files via upload

---
+++ EvenVsOdd.py
@@ -0,0 +1,6 @@
+Number = int(input("Please enter your desired whole number to see whether the nubmber is even or odd: "))
+
+if Number%2 == 0:
+    print("The number is Even")
+else:
+    print("The number is odd")
